9e1b47fb2f6e06ad58e0cde9e03af8b698644578
SiddharthGoswami1997 <siddharth.go1997@gmail.com>
2024-02-15 16:38:39 +0530

    palindrome is same as string reversing

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+let str="nayan"
+let newStr=""
+for(let i=str.length;i>=1;i--){
+
+newStr+=str[i-1]
+
+}
+console.log(str)
+console.log(newStr)
+if(str===newStr){
+console.log("It is a Palindrome",str)
+
+}
+else{
+    console.log("not a Palindrome",str)
+}
