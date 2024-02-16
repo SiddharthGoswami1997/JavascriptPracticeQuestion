0e45d2a455e802e500d561751fc66feb2806a195
SiddharthGoswami1997 <siddharth.go1997@gmail.com>
2024-02-16 15:25:56 +0530

    3 ways to solve febonacci

---
+++ febonacci.js
@@ -0,0 +1,54 @@
+// let a=0;
+// let b=1;
+// let d=[]
+// for(let i=20;i>=0;i--){
+//     let c=a+b;
+//     a=b;
+//     b=c;
+//        d.splice(0,0,c)
+// }
+// d.push(1,0)
+// let s=[]
+// for(let x=d.length;x>=1;x--){
+// s.push(d[x-1])
+// }
+// console.log(s)
+
+
+
+
+//// Easy way
+// let a=0;
+// let b=1;
+// console.log(a)
+// console.log(b)
+// for(let i=20;i>=0;i--){
+//         let c=a+b;
+//         a=b;
+//         b=c;
+//     console.log(c)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// most easy way
+// let a=0;
+// let b=1;
+// let d
+// for(let i=20;i>=0;i--){
+//     console.log(a)
+//     d=a+b
+//     a=b
+//     b=d
+
+// }
